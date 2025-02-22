1bdfadd7db72e29a991307ad8340e632ba159403
developerKamilla <135846169+developerKamilla@users.noreply.github.com>
2025-02-22 21:18:09 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from window import Window
+import sys
+
+if __name__ == "__main__":
+    app: QApplication = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
