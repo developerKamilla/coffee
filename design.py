9449df27d1c47aa730129676dc0daad3f6ef3a4d
developerKamilla <135846169+developerKamilla@users.noreply.github.com>
2025-02-22 21:19:40 +0300

    design.py

---
+++ design.py
@@ -0,0 +1,19 @@
+from PyQt6 import QtCore, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.table = QtWidgets.QTableView(self.centralwidget)
+        self.table.setGeometry(QtCore.QRect(10, 30, 780, 560))
+        self.table.setObjectName("table")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "coffee"))
